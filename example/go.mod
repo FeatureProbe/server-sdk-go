af6c5877d25ab900deb6328c7d3ab855ba9cc828
ssebo <zhangshibo@featureprobe.com>
2022-06-18 08:28:31 +0800

    feat: add example project

---
+++ example/go.mod
@@ -0,0 +1,5 @@
+module go-example
+
+go 1.13
+
+require github.com/featureprobe/server-sdk-go v0.0.0-20220617081056-b9a920b322a5
---
+++ example/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/featureprobe/server-sdk-go"
+)
+
+func main() {
+	config := featureprobe.FPConfig{
+		RemoteUrl:       "http://127.0.0.1:4001",
+		ServerSdkKey:    "server-8ed48815ef044428826787e9a238b9c6a479f98c",
+		RefreshInterval: 1000,
+		WaitFirstResp:   true,
+	}
+	fp, err := featureprobe.NewFeatureProbe(config)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	user := featureprobe.NewUser("user")
+
+	for {
+		detail := fp.StrDetail("color_ab_test", user, "black")
+		fmt.Println(detail)
+		time.Sleep(time.Duration(5) * time.Second)
+	}
+}
